92640ce6bee5f38ebdaad4413a20d9fb97545e01
olechkakulic <olga.loginova@metalab.ifmo.ru>
2023-02-13 03:13:06 +0300

    add

---
+++ task25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+double getCount(int x, int y)
+{
+    double result = Math.Pow(x, y);
+    return result;
+}
+int getNumber(String text)
+{
+    Console.WriteLine(text);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+int x = getNumber("Введите любое число");
+int y = getNumber("Введите степень, в которую хотите возвести число ");
+Console.Write(x + ", " + y + " -> " + getCount(x, y));
---
+++ task27/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+void getSumm(int x){
+    int summa=0;
+    while(x>0){
+        summa+=x%10;
+        x/=10;
+    }
+    Console.WriteLine(summa);
+
+}
+int getNumber(String text){
+    Console.WriteLine(text);
+    return(int.Parse(Console.ReadLine()));
+}
+int x = getNumber("Введите любое число");
+getSumm(x);
+
+
+// void getLength(int x){
+//     int index=0;
+//     while(x>0){
+//         x=x/10;
+//         index++;
+//     }
+//     Console.WriteLine(index);
+
+// }
+// int getNumber(String text){
+//     Console.WriteLine(text);
+//     return(int.Parse(Console.ReadLine()));
+// }
+// int x = getNumber("Введите любое число");
+// getLength(x);
